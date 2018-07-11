59f8e7b9807c4a02e56a7270ee84f92349b89765
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2018-07-11 18:07:00 +0200

    - added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+speedrun - A toolkit for quick-and-clean machine learning experiments with Pytorch and beyond.
+"""
+
+import setuptools
+
+
+setuptools.setup(
+    name="speedrun",
+    author="Nasim Rahaman",
+    author_email="nasim.rahaman@iwr.uni-heidelberg.de",
+    license='GPL-v3',
+    description="Toolkit for machine learning experiment management.",
+    version="0.1",
+    packages=setuptools.find_packages(),
+)
