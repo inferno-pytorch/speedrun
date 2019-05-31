f441d10face34f1804055f18f901377203b78b63
Roman Remme <roman.remme@gmx.de>
2019-05-31 16:21:36 +0200

    move firelight to logging

--- speedrun/__init__.py
+++ speedrun/__init__.py
@@ -1,14 +1,14 @@
 from .core import BaseExperiment
 
 # Submodules
-from .logging import io_logging, tensorboard, plotting
+from .logging import io_logging, tensorboard, plotting, firelight
 from .utils import py_utils, yaml_utils
 
 # Objects
 from .inferno import InfernoMixin
 from .resource import WaiterMixin
-from .firelight import FirelightMixin
 
 from .logging.tensorboard import TensorboardMixin
 from .logging.io_logging import IOMixin
+from .logging.firelight import FirelightMixin
 
