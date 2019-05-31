a0649e6882f239bd6bc252c09c6f49c5be84fb7d
Roman Remme <roman.remme@gmx.de>
2019-05-31 14:22:18 +0200

    add command line autocomplete for --config

--- setup.py
+++ setup.py
@@ -3,6 +3,120 @@ speedrun - A toolkit for quick-and-clean machine learning experiments with Pytor
 """
 
 import setuptools
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+import os
+import sys
+import site
+
+
+# https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+# https://stackoverflow.com/questions/36187264/how-to-get-installation-directory-using-setuptools-and-pkg-ressources
+def binaries_directory():
+    """Return the installation directory, or None"""
+    if '--user' in sys.argv:
+        paths = (site.getusersitepackages(),)
+    else:
+        py_version = '%s.%s' % (sys.version_info[0], sys.version_info[1])
+        paths = (s % (py_version) for s in (
+            sys.prefix + '/lib/python%s/site-packages/',
+            sys.prefix + '/local/lib/python%s/site-packages/',
+            sys.prefix + '/lib/python%s/dist-packages/',
+            sys.prefix + '/local/lib/python%s/dist-packages/',
+            '/Library/Python/%s/site-packages/',
+        ))
+
+    for path in paths:
+        if os.path.exists(path):
+            return path
+    print('no installation path found', file=sys.stderr)
+    return None
+
+
+def setup_shell_init(rcfile, init_lines):
+    with open(rcfile, 'r') as file:
+        lines = file.readlines()
+    start_marker = '# >>> speedrun >>>\n'
+    end_marker = '# <<< speedrun <<<\n'
+    start, end = None, None
+    if start_marker in lines:
+        start = lines.index(start_marker)
+    if end_marker in lines:
+        end = lines.index(end_marker)
+    override_previous = (start is not None) and (end is not None) and (end > start)
+    if override_previous is True:
+        print(f'Overriding previous speedrun init in {rcfile}')
+        new_lines = lines[:start]
+    else:
+        print(f'Adding new speedrun init to {rcfile}')
+        new_lines = lines
+    new_lines.append(start_marker)
+    new_lines.extend(init_lines)
+    new_lines.append(end_marker)
+    if override_previous:
+        new_lines.extend(lines[end+1:])
+    with open(rcfile, 'w') as file:
+        file.writelines(new_lines)
+
+
+def setup_shell_inits(speedrun_path):
+    init_lines = [
+        "# !! Contents within this block are managed by speedrun's install script !!\n"
+        '# Enable command line completion for config keys\n',
+        f'source {os.path.join(speedrun_path, "autocomplete", "init_completion")}\n',
+    ]
+    for file in [os.path.expanduser(f) for f in ['~/.bashrc', '~/.zshrc']]:
+        if os.path.isfile(file) and query_yes_no(
+                f'Should custom command line completion be enabled in {file} ?'):
+            setup_shell_init(file, init_lines)
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        setup_shell_inits(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'speedrun'))
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        setup_shell_inits(os.path.join(binaries_directory(), 'speedrun'))
 
 
 setuptools.setup(
@@ -14,4 +128,11 @@ setuptools.setup(
     version="0.1",
     install_requires=['pyyaml>=3.12'],
     packages=setuptools.find_packages(),
+    package_data={
+        '': ['init_completion'],
+    },
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
 )
---
+++ speedrun/autocomplete/complete.py
@@ -0,0 +1,144 @@
+from sys import argv
+# this updates the default loader
+import yaml
+import os
+
+# TODO: add completion to --update
+
+working_directory = argv[1]
+current_command = argv[2]
+current_word_id = int(argv[3])
+
+# ------------- check if config completion should be applied ----------------
+
+current_command = current_command.split()
+
+#print('test_option')
+
+if len(current_command) == 0:
+    exit(0)
+
+# get the argument that is currently modified
+try:
+    current_arg = current_command[current_word_id]
+except IndexError:
+    exit(0)
+
+if not current_arg.startswith('--config.'):
+    # not altering arguments in the config
+    exit(0)
+path_in_config = current_arg.split('.')[1:]
+
+# get the specified config file
+try:
+    inherit_pos = current_command.index('--inherit')
+except ValueError:
+    # no --inherit found
+    exit(0)
+if inherit_pos == len(current_command) - 1:
+    # --inherit at the end of the command
+    exit(0)
+
+# convert config_path to an absolute path
+config_path = os.path.expanduser(current_command[inherit_pos + 1])
+if not config_path.startswith('/'):
+    config_path = os.path.join(working_directory, config_path)
+if not os.path.isfile(config_path):
+    # an experiment directory is specified
+    config_path = os.path.join(config_path, 'Configurations', 'train_config.yml')
+    if not os.path.isfile(config_path):
+        # config file not found
+        exit(0)
+
+
+# ------------- modify yaml loader to only load the nested dict structure ----------------
+
+
+def none_constructor(*args, **_):
+    return None
+
+
+yaml.add_constructor('!Add', none_constructor)
+yaml.add_constructor('!Mul', none_constructor)
+yaml.add_constructor('!Sub', none_constructor)
+yaml.add_constructor('!Div', none_constructor)
+yaml.add_constructor('!NumpyArray', none_constructor)
+yaml.add_constructor('!TorchTensor', none_constructor)
+yaml.add_constructor('!Hyperopt', none_constructor)
+
+
+def override_constructor(loader, node):
+    if isinstance(node, yaml.MappingNode):
+        return loader.construct_mapping(node)
+    else:
+        exit(0)
+
+
+yaml.add_constructor('!Override', override_constructor)
+
+yaml.add_constructor('!Del', none_constructor)
+
+
+def override_constructor(loader, node):
+    if isinstance(node, yaml.MappingNode):
+        return loader.construct_mapping(node)
+    else:
+        # normally, an error is raised
+        exit(0)
+
+
+yaml.add_constructor('!Override', override_constructor)
+
+
+def temp_obj_constructor(loader, tag_suffix, node):
+    if isinstance(node, yaml.ScalarNode):
+        return loader.construct_scalar(node)
+    elif isinstance(node, yaml.SequenceNode):
+        return loader.construct_sequence(node)
+    elif isinstance(node, yaml.MappingNode):
+        return loader.construct_mapping(node)
+    else:
+        raise NotImplementedError('Node: ' + str(type(node)))
+
+
+yaml.add_multi_constructor('!Obj:', temp_obj_constructor)
+yaml.add_multi_constructor('!OverrideObj:', temp_obj_constructor)
+
+
+class StructureLoader(yaml.Loader):
+    """
+    Loader for command line auto completion: stops when a non-dictionary is encountered
+    Examples:
+
+    """
+    def construct_sequence(self, node, deep=False):
+        return []
+
+
+# ------------- load and process the config ----------------
+
+# load the config
+try:
+    with open(config_path, 'r') as f:
+        config = yaml.load(f, Loader=StructureLoader)
+except:
+    # config could not be loaded for some reason
+    exit(0)
+
+# find the current options
+for key in path_in_config[:-1]:
+    try:
+        config = config[key]
+    except ValueError:
+        exit(0)
+if not isinstance(config, dict):
+    exit(0)
+
+# print the options to be filtered by bash (maybe filter them here?)
+for key in config.keys():
+    if str(key).startswith(path_in_config[-1]):
+        completion_option = '.'.join(['--config'] + path_in_config[:-1]) + '.' + str(key)
+        if isinstance(config[key], dict):
+            # automatically add the dot if there are further nested options
+            completion_option += '.'
+        print(completion_option)
